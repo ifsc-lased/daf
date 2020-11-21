9a077888c478f44782c51c3779590ff84ca52b01
jaquerifr <jaquerifr@gmail.com>
2020-11-21 09:43:54 -0300

    iniciando repositório

--- assets/js/contact_me.js
+++ assets/js/contact_me.js
@@ -8,7 +8,7 @@ $(function() {
     submitSuccess: function($form, event) {
       event.preventDefault(); // prevent default submit behaviour
       // get values from FORM
-	  var url = "https://formspree.io/" + "your-email@example.com";
+	  var url = "https://formspree.io/" + "lased@ifsc.edu.br";
       var name = $("input#name").val();
       var email = $("input#email").val();
       var phone = $("input#phone").val();
